0e08ceee3916d2ba1c3c6023096d9164262d27d4
mustafa <mstfssk@gmail.com>
2014-08-22 16:22:32 +0300

    Kullanıcı girdisi istendi

--- kelimetahmin.py
+++ kelimetahmin.py
@@ -1,1 +1,12 @@
-from sys import argv
+#coding: utf-8
+my_word = "deneme"
+
+print "------"
+inp = raw_input("bir harf giriniz:")
+
+
+def find_index_of_letter(word, letter):
+    if letter in word:
+        return word.index(letter)
+
+print find_index_of_letter(my_word, inp)
