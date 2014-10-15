fa985b237242cdf41607e240e6dd60591ddf82aa
mustafa <mstfssk@gmail.com>
2014-10-16 01:32:56 +0300

    Text indexation

--- indexOfWord.py
+++ indexOfWord.py
@@ -1,17 +1,19 @@
 text = """bugun hava cok guzel. bu yuzden bugun disari cikip hava almak istiyorum.
-cok yorgunum ama bir daha bu havayi bulacagimi sanmiyorum."""
+cok yorgunum. ama bir daha bu havayi bulacagimi sanmiyorum."""
 
 def indexOfWord(textName):
     d = {}
     sentenceList = textName.split(".")
-    wordNumber = -1
+    wordNumber = 0
     for sentence in sentenceList:
         sentenceIndex = []
         for word in sentence.split():
-            wordNumber += 1
             sentenceIndex.append(wordNumber)
             if not word in d:
-                d[word] = (wordNumber, sentenceIndex)
+                d[word] = [(wordNumber, sentenceIndex)]
+            else:
+                d[word].append((wordNumber, sentenceIndex))
+            wordNumber += 1
     return d
 
 
